2d33c72d6ef6f00cc0474771720d851e6f3f92e4
Asif Islam <77113884+asif4318@users.noreply.github.com>
2024-03-07 16:51:48 -0500

    Added ORM + Docker Part 2

---
+++ backend/app/main.py
@@ -0,0 +1,43 @@
+from typing import Optional
+
+# One line of FastAPI imports here later 👈
+from contextlib import asynccontextmanager
+from fastapi import FastAPI
+from sqlmodel import Field, Session, SQLModel, create_engine, select
+
+
+class User(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    name: str = Field(index=True)
+
+
+engine = create_engine("postgresql://user:password@localhost:5432", echo=True)
+
+
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
+
+
+app = FastAPI()
+
+
+@app.on_event("startup")
+async def startup_event():
+    create_db_and_tables()
+    print("STARTED")
+
+
+@app.post("/users/")
+def create_user(user: User):
+    with Session(engine) as session:
+        session.add(user)
+        session.commit()
+        session.refresh(user)
+        return user
+
+
+@app.get("/users/")
+def read_users():
+    with Session(engine) as session:
+        heroes = session.exec(select(User)).all()
+        return heroes
