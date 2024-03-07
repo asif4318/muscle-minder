3b83eef2731e741eb22573d13c0b8d3fe9f61c4d
Asif Islam <77113884+asif4318@users.noreply.github.com>
2024-03-07 18:25:03 -0500

    Commented out muscles property in Excercise (join/link table is required for many-to-many relationship)

--- backend/app/routers/models/excercise.py
+++ backend/app/routers/models/excercise.py
@@ -6,4 +6,4 @@ from .muscle import Muscle
 class Excercise(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     name: str = Field(index=True)
-    muscles: Optional[list[Muscle]] = Field()
+    # muscles: Optional[list[Muscle]] = Field()
