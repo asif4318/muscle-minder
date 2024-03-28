114eed3952545605ff4a24516de87eae3c19cebf
austinnickse <115114635+austinnickse@users.noreply.github.com>
2024-03-27 21:45:12 -0400

    Update excercise.py
    Adds instruction str as an optional attribute of an exercise

--- backend/app/routers/models/excercise.py
+++ backend/app/routers/models/excercise.py
@@ -7,5 +7,6 @@ from .exercise_muscle_link import ExcerciseMuscleLink
 class Excercise(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     name: str = Field(index=True)
+    instruction: Optional[str] = None
     # muscles: List["Muscle"] = Relationship(
     #     back_populates="muscle", link_model=ExcerciseMuscleLink)
