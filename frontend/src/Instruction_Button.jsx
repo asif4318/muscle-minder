308a0a3d4a141c8487c063f1ba6c5b1fcd904cd9
austinnickse <115114635+austinnickse@users.noreply.github.com>
2024-03-28 05:02:17 -0400

    Update Instruction_Button.jsx again
    Adds it so there is an option if the instruction field was never filled out that alerts the user that there is no instructions

--- frontend/src/Instruction_Button.jsx
+++ frontend/src/Instruction_Button.jsx
@@ -9,9 +9,12 @@ export function Link_Button() {
     if (exercise.instruct.match(/youtube.com/) && (exercise.instruct.startsWith('https://www.youtube.com') || exercise.instruct.startsWith('youtube.com'))) {
       openURL(exercise.instruct) //automatically ignores everything after first word so long as it is the link - above statements should make sure it is a link to youtube
     }
-    else { //Opens a window that has the entire instructions printed out - not themed
+    else if (exercise.instruct != None) { //Opens a window that has the entire instructions printed out - not themed - only if the string is not empty
       var wnd = window.open("about:blank", "", "_blank");
       wnd.document.write(exercise.instruct);
+    }
+    else {
+      alert("This workout has no instructions")
     }
 
     return (
