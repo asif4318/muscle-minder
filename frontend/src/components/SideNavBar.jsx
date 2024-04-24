08150ff76177346008a800124048cd05324bffca
David A <152811793+rage236@users.noreply.github.com>
2024-04-24 14:10:57 -0400

    SideNavBar now only displays buttons if user is logged in
    user is now required to be logged in to access the log, activity, and dashboard pages

--- frontend/src/components/SideNavBar.jsx
+++ frontend/src/components/SideNavBar.jsx
@@ -1,9 +1,22 @@
 import { GridItem, Container, Heading, Button, VStack } from "@chakra-ui/react";
 import { ArrowBackIcon } from "@chakra-ui/icons";
-import { signOut } from "firebase/auth";
+import { signOut, onAuthStateChanged } from "firebase/auth";
 import { auth } from "../firebase_config";
+import { useState, useEffect } from "react"
 
 const SideNavBar = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(0)
+  
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setIsLoggedIn(1)
+      } else {
+        setIsLoggedIn(0)
+      }
+    });
+  }, []);
+  if(isLoggedIn){
   return (
     <GridItem bg="purple.600" height="100%">
       <VStack spacing={10}>
@@ -34,6 +47,15 @@ const SideNavBar = () => {
       </VStack>
     </GridItem>
   );
-};
+}else{
+  return (
+    <GridItem bg="purple.600" height="100%">
+      <VStack spacing={10}>
+      </VStack>
+    </GridItem>
+  )
+
+}
+}
 
 export default SideNavBar;
